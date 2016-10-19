9e1fa6fbc3e54eb4820f0caa34e3897f4cde217c
Tim White <timwhite47@gmail.com>
2016-10-18 23:08:41 -0700

    button onclick

--- src/button.js
+++ src/button.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import cx from 'classnames';
 
 class Button extends React.Component {
 
   render() {
-    const { type, value } = this.props;
+    const { type, value, className, onClick } = this.props;
+    const cutestrapButtonType = `btn--${type}`;
+    const buttonClass = cx(cutestrapButtonType, className);
 
     return (
-      <button className={`btn--${type}`}>{value}</button>
+      <button className={buttonClass} onClick={onClick}>{value}</button>
     );
   }
 
 }
 
 Button.propTypes = {
+  className: React.PropTypes.any,
+  onClick: React.PropTypes.func,
   type: React.PropTypes.oneOf(['primary', 'secondary', 'link']),
   value: React.PropTypes.string,
 };
---
+++ test/button-test.js
@@ -0,0 +1,32 @@
+import Button from '../src/button';
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
+let wrapper;
+let onClick;
+describe('<Button />', function () {
+  beforeEach(function () {
+    onClick = sinon.spy();
+    wrapper = mount(<Button className='foobar' onClick={onClick} value='CLICK ME' />);
+  });
+
+  it('renders with correct classname', function () {
+    expect(wrapper.find('button')).to.have.className('btn--primary');
+  });
+
+  it('renders with passed in className', function () {
+    expect(wrapper.find('button')).to.have.className('foobar');
+  });
+
+  describe('onClick', function () {
+    beforeEach(function (done) {
+      wrapper.find('button').simulate('click');
+      done();
+    });
+
+    it('triggers onClick prop', function () {
+      expect(onClick.callCount).to.equal(1);
+    });
+  });
+});
